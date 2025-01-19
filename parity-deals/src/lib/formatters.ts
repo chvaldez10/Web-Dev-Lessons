6394b25b92772c5eab2ab347a9f7fbb443d77f05
chvaldez10 <christianvaldez021@gmail.com>
2025-01-18 18:33:12 -0700

    delete project

--- parity-deals/src/lib/formatters.ts
+++
@@ -1,7 +0,0 @@
-const compactNumberFormatter = new Intl.NumberFormat(undefined, {
-  notation: "compact",
-})
-
-export function formatCompactNumber(number: number) {
-  return compactNumberFormatter.format(number)
-}
