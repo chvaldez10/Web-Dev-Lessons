e068259d7a88f929825710bf25247b2276dd6892
chvaldez10 <christianvaldez021@gmail.com>
2025-03-30 21:04:39 -0600

    migrate create users

--- ai-assistant/src/app/provider.tsx
+++ ai-assistant/src/app/provider.tsx
@@ -2,15 +2,20 @@
 import React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ConvexReactClient, ConvexProvider } from "convex/react";
 
 export function Provider({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-      <NextThemesProvider attribute="class" defaultTheme="light" enableSystem>
-        {children}
-      </NextThemesProvider>
-    </GoogleOAuthProvider>
+    <ConvexProvider client={convex}>
+      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+        <NextThemesProvider attribute="class" defaultTheme="light" enableSystem>
+          {children}
+        </NextThemesProvider>
+      </GoogleOAuthProvider>
+    </ConvexProvider>
   );
 }
