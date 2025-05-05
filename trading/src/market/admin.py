1ea68ff9136fec8b235adb4dc9c5dc4f1d9e3358
chvaldez10 <christianvaldez021@gmail.com>
2025-05-04 20:23:54 -0600

    delete old folders

--- trading/src/market/admin.py
+++
@@ -1,8 +0,0 @@
-import zoneinfo
-from django.contrib import admin
-from django.utils import timezone
-
-from .models import Company, StockQuote
-
-admin.site.register(Company)
-admin.site.register(StockQuote)
