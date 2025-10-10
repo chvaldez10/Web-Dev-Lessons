0dff078f0540543885e61d30b3f4db89db3b420e
chvaldez10 <christianvaldez021@gmail.com>
2025-10-10 16:11:44 -0600

    add shad cn

--- prostore/src/app/not-found.tsx
+++ prostore/src/app/not-found.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
-// import { Button } from '@/components/ui/button';
-// import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
@@ -17,9 +17,9 @@ const NotFoundPage = () => {
       <div className="p-6 w-1/3 rounded-lg shadow-md text-center">
         <h1 className="text-3xl font-bold mb-4">Not Found</h1>
         <p className="text-destructive">Could not find requested page</p>
-        {/* <Button variant='outline' className='mt-4 ml-2' asChild>
-          <Link href='/'>Back To Home</Link>
-        </Button> */}
+        <Button variant="outline" className="mt-4 ml-2" asChild>
+          <Link href="/">Back To Home</Link>
+        </Button>
       </div>
     </div>
   );
