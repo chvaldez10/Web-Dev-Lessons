8351a7065b1df8245665a69706cdf4586d9c9439
chvaldez10 <christianvaldez021@gmail.com>
2025-10-11 19:48:01 -0600

    add product list

--- prostore/src/app/(root)/page.tsx
+++ prostore/src/app/(root)/page.tsx
@@ -1,7 +1,12 @@
+import { sampleData } from "@/db/sample-data";
+import ProductList from "@/components/shared/product/ProductList";
+
 const Homepage = async () => {
+  console.log(sampleData);
   return (
     <div>
       <h1>home page</h1>
+      <ProductList data={sampleData.products} />
     </div>
   );
 };
