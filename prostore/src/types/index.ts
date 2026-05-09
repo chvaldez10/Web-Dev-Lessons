0326fd46732cd84b2e9db4fd5b9d20af6103eb03
chvaldez10 <christianvaldez021@gmail.com>
2026-05-09 09:57:45 -0600

    delete prostore

--- prostore/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { insertProductSchema } from "@/lib/validators";
-
-export type Product = z.infer<typeof insertProductSchema> & {
-  id: string;
-  rating: string;
-  numReviews: number;
-  createdAt: Date;
-};
